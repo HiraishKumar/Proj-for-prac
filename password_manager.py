dabf6242a1eb9de7f5dfd4eeb4796be05a7afc62
Killy <87876086+KillyDaviel@users.noreply.github.com>
2023-07-29 15:21:57 +0530

    encryption and decryption function

---
+++ password_manager.py
@@ -0,0 +1,31 @@
+import string
+def Encrypter(initial_string:str,cypher:int=3)-> str:
+    '''
+    shift the postion of the element in the string base {cypher} No. of times
+    '''
+    encrypt=''    
+    string_base=[i for i in (string.ascii_letters+string.digits+' ')]
+    #'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 '
+    for letter in initial_string:
+        new_letter_index=string_base.index(letter)+cypher
+        if new_letter_index>=len(string_base)-1:
+            new_letter_index-=len(string_base)
+            encrypt+=string_base[new_letter_index]
+        else:
+            encrypt+=string_base[new_letter_index]
+
+    return encrypt   
+ 
+def Decrypter(initial_string:str,cypher:int=3)-> str:
+    '''
+    shift the postion of the element in the string base BACK {cypher} No. of times
+    (to mentain integrity decrypt with the same cypher as encryption)
+    '''   
+    decrypt=''
+    string_base=[i for i in (string.ascii_letters+string.digits+' ')]
+    #'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 '
+    for letter in initial_string:        
+        new_letter_index=string_base.index(letter)-cypher
+        decrypt+=string_base[new_letter_index]
+            
+    return decrypt
