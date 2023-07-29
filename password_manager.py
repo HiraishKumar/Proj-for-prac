88e74d5670cc7491ae859c84738d8eab14c85030
Killy <87876086+KillyDaviel@users.noreply.github.com>
2023-07-29 16:25:50 +0530

    working on it

--- password_manager.py
+++ password_manager.py
@@ -9,8 +9,11 @@ def Encrypter(initial_string:str,cypher:int=3)-> str:
     for letter in initial_string:
         new_letter_index=string_base.index(letter)+cypher
         if new_letter_index>=len(string_base)-1:
-            new_letter_index-=len(string_base)
-            encrypt+=string_base[new_letter_index]
+            if letter in string_base:
+                new_letter_index-=len(string_base)
+                encrypt+=string_base[new_letter_index]
+            else:
+                pass
         else:
             encrypt+=string_base[new_letter_index]
 
@@ -29,3 +32,9 @@ def Decrypter(initial_string:str,cypher:int=3)-> str:
         decrypt+=string_base[new_letter_index]
             
     return decrypt
+
+import pandas as pd
+df = pd.read_csv('password_database.csv')
+
+df.loc[:, 'Password'] = Encrypter(df.loc[:, 'Password'])
+df.head(10)
