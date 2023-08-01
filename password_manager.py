381cbf73bca1010ddbc531c96af2f3697e0e4e9d
Killy <87876086+KillyDaviel@users.noreply.github.com>
2023-08-01 17:37:08 +0530

    re-adjustment

--- password_manager.py
+++ password_manager.py
@@ -1,4 +1,34 @@
 import string
+import pandas as pd
+df = pd.read_csv(r'D:\python\Projects\Encrypted_password_database .csv')
+# def Unique_Element_Finder(colomn_name:str,colomn_index:int)->list:
+'''Returns the list of unique elemets in a colomn(identified by its {colomn_name} and {colomn_index}) of a pandas datafram '''
+Unique_Website_list=[]
+for i in [df.iloc[i,0] for i in range(len(df['Facebook.com']))]:
+    if i in Unique_Website_list:
+        pass
+    else:
+        Unique_Website_list.append(i)
+            
+    # return Unique_Website_list
+print(Unique_Website_list)
+
+# def Unique_Element_Finder(df: pd.DataFrame, column_name: str) -> list:
+#     '''Returns the list of unique elements in a column (identified by its {column_name}) of a pandas DataFrame.'''
+#     unique_elements_list = [df[column_name].unique().tolist()]
+#     return unique_elements_list
+
+# def Unique_Element_Finder(df: pd.DataFrame, column_index: int) -> list:
+#     '''Returns the list of unique elements in a column (identified by its {column_index}) of a pandas DataFrame.'''
+#     unique_elements_list = df.iloc[:, column_index].unique().tolist()
+#     return unique_elements_list
+
+
+
+
+Website_name='Facebook.com'
+filtered_df = df[df['Website'] == Website_name]
+
 def Encrypter(initial_string:str,cypher:int=3)-> str:
     '''
     shift the postion of the element in the string base {cypher} No. of times
@@ -18,7 +48,6 @@ def Encrypter(initial_string:str,cypher:int=3)-> str:
             encrypt+=letter    
 
     return encrypt   
- 
 def Decrypter(initial_string:str,cypher:int=3)-> str:
     '''
     shift the postion of the element in the string base BACK {cypher} No. of times
@@ -36,7 +65,17 @@ def Decrypter(initial_string:str,cypher:int=3)-> str:
     return decrypt
 
 
-a = Encrypter("G@m3OnNow")
-b = Decrypter(a)
-print(a)
-print(b)
+lst1=[filtered_df.iloc[i,1] for i in range(len(filtered_df['Username']))]
+lst2=[Decrypter(filtered_df.iloc[i,2]) for i in range(len(filtered_df['Password']))]
+
+
+# encrypted=list(map(Encrypter,lst1))
+# decrypted=list(map(Decrypter,encrypted))
+
+# print(encrypted)
+# print(decrypted)
+# print(lst2)
+
+paired_list=list(zip(lst1,lst2))
+for i in paired_list:
+    print('Facebook.com','->',i[0],'->',i[1])
