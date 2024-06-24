e53b550fdf80c49cef0fe3e682264c80385a4d7b
Killy <87876086+KillyDaviel@users.noreply.github.com>
2024-06-25 01:32:19 +0530

    TODO get serial workign

--- Ctrl_Vol/main.py
+++ Ctrl_Vol/main.py
@@ -1,17 +1,47 @@
 import serial
 import time
+from tkinter import *
+from tkinter import ttk
 
-ser = serial.Serial('COM8',9600)
-time.sleep(2)
+# ser = serial.Serial('COM8',9600)
+# time.sleep(2)
 
-try:
-    print("Started Logging")
-    while True:
-        if ser.in_waiting > 0:
-            line = ser.readline().decode('utf-8').strip()
-            print(line)
-except KeyboardInterrupt:
-    print("Logging Stopped")
-finally:
-    ser.close()
-    
+# try:
+#     print("Started Logging")
+#     while True:
+#         if ser.in_waiting > 0:
+#             line = ser.readline().decode('utf-8').strip()
+#             print(line)
+# except KeyboardInterrupt:
+#     print("Logging Stopped")
+# finally:
+#     ser.close()
+
+root = Tk()
+root.title("Serial Reader")
+root.geometry("500x500")
+
+
+mainframe = ttk.Frame(root,padding=("3 3 12 12"))
+mainframe.grid(row=0,column=0)
+
+root.columnconfigure(0,weight=1)
+root.rowconfigure(0,weight=1)
+
+
+ttk.Label(mainframe,text="Ser1",background="Grey").grid(row=1,column=1,sticky=(W,E))
+ttk.Label(mainframe,text="Ser2",background="Grey").grid(row=1,column=2,sticky=(W,E))
+ttk.Label(mainframe,text="Ser3",background="Grey").grid(row=1,column=3,sticky=(W,E))
+ttk.Label(mainframe,text="Ser4",background="Grey").grid(row=1,column=4,sticky=(W,E))
+ttk.Label(mainframe,text="Ser5",background="Grey").grid(row=1,column=5,sticky=(W,E))
+
+
+ttk.Label(mainframe,text="ser1").grid(row=2,column=1)
+ttk.Label(mainframe,text="ser2").grid(row=2,column=2)
+ttk.Label(mainframe,text="ser3").grid(row=2,column=3)                                                           
+ttk.Label(mainframe,text="ser4").grid(row=2,column=4)
+ttk.Label(mainframe,text="ser5").grid(row=2,column=5)
+
+
+
+root.mainloop()
--- Ctrl_Vol/test.py
+++ Ctrl_Vol/test.py
@@ -11,28 +11,32 @@ def Calculate(*args) -> None:
 
 root = Tk()
 root.title('Feet to meters')
+root.geometry('330x110')
+root.resizable(False, False)
 
-mainframe = ttk.Frame(root,padding="10 10 10 10")
-mainframe.grid(column=0,row=0,sticky=(N,W,E,S))
+mainframe = ttk.Frame(root,padding="3 3 12 12")
+mainframe.grid(column=0,row=0)
 
-root.columnconfigure(0,weight=2)
-root.rowconfigure(0,weight=2)
+root.columnconfigure(0,weight=1)
+root.rowconfigure(0,weight=1)
 
 feet=StringVar()
-feet_entry = ttk.Entry(mainframe,width=7,textvariable=feet)
-feet_entry.grid(column=2,row=1,sticky=(W,E))
-ttk.Label(mainframe,text="Feet").grid(column=3,row=1,sticky=W)
+feet_entry = ttk.Entry(mainframe,textvariable=feet)
+feet_entry.grid(column=2,row=1)
+ttk.Label(mainframe,text="Feet",).grid(column=3,row=1,sticky=W)
 
 
 meter = StringVar()
-ttk.Label(mainframe,text="is Equivalent to ").grid(column=1,row=2,sticky=E)
-meter_entry = ttk.Entry(mainframe,textvariable=meter)
-meter_entry.grid(column=2,row=2,sticky=(W,E))
+ttk.Label(mainframe,text="is Equivalent to ").grid(column=1,row=2)
+meter_entry = ttk.Entry(mainframe,textvariable=meter,state=DISABLED)
+meter_entry.grid(column=2,row=2)
 ttk.Label(mainframe,text="Meter").grid(column=3,row=2,sticky=W)
 
 
-ttk.Button(mainframe,text="calculate",command=Calculate).grid(row=3,column=3,sticky=(W,E))
+ttk.Button(mainframe,text="calculate",command=Calculate).grid(row=3,column=3,sticky=(N,W,E,S))
 
+for child in mainframe.winfo_children(): 
+    child.grid_configure(padx=5, pady=5)
 feet_entry.focus()
 root.bind("<Return>",Calculate)
 
